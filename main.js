4d404cd5f1c8fdd635dbd38d62ef02efe4ffd89d
samueledwardswebdev <69369854+samueledwardswebdev@users.noreply.github.com>
2021-09-02 17:29:09 +0100

    1st

---
+++ main.js
@@ -0,0 +1,16 @@
+// const openMenu = document.querySelector('.openMenu')
+// const closeMenu = document.querySelector('.closeMenu')
+// const mainMenu = document.querySelector('.mobileMenu')
+
+// openMenu.addEventListener('click', open);
+// closeMenu.addEventListener('click', close);
+
+// function open () {
+//   mainMenu.style.display = "flex";
+//   mainMenu.style.top = "0";
+// }
+
+// function close () {
+//   mainMenu.style.display = "none";
+//   mainMenu.style.top = "-100%";
+// }
